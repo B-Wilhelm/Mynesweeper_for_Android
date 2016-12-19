54d143d4bc234e028b6c4a7f504fc8d799db73cc
Brett <bwilhelm@iastate.edu>
2016-12-18 22:23:10 -0600

    Changed to blue background, centered image

--- core/src/com/tjstudios/mynesweeper/Mynesweeper.java
+++ core/src/com/tjstudios/mynesweeper/Mynesweeper.java
@@ -9,19 +9,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Mynesweeper extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+    private static int WIN_HEIGHT;
+    private static int WIN_WIDTH;
+
+    // Blue Background
+    private static float BKGD_RED = .22f;
+    private static float BKGD_GREEN = .624f;
+    private static float BKGD_BLUE = .761f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+        WIN_HEIGHT = Gdx.graphics.getHeight();
+        WIN_WIDTH = Gdx.graphics.getWidth();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(BKGD_RED, BKGD_GREEN, BKGD_BLUE, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, WIN_WIDTH/2-img.getWidth()/2, WIN_HEIGHT/2-img.getHeight()/2);
 		batch.end();
 	}
 	
--- desktop/src/com/tjstudios/mynesweeper/desktop/DesktopLauncher.java
+++ desktop/src/com/tjstudios/mynesweeper/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.tjstudios.mynesweeper.Mynesweeper;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Mynesweeper";
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new Mynesweeper(), config);
 	}
 }
