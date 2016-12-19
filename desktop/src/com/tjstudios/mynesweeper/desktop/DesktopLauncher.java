ebfafc02593385ee5a9eb086e1a2b776ac6e0554
Brett <bwilhelm@iastate.edu>
2016-12-18 19:59:25 -0600

    init

---
+++ desktop/src/com/tjstudios/mynesweeper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tjstudios.mynesweeper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tjstudios.mynesweeper.Mynesweeper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mynesweeper(), config);
+	}
+}
