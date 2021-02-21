d0ba30ad8daa91147aafd5e8afb13223c8646104
Brett <bwilhelm@techie.com>
2021-02-21 02:10:24 -0600

    Set up everything to work with modern versions of gradle, intellij, updated libgdx and plugins, set min platform of android version to 7.1.1, and got rid of a bunch of outdated code that was breaking things.

--- android/src/com/tjstudios/mynesweeper/AndroidLauncher.java
+++ android/src/com/tjstudios/mynesweeper/AndroidLauncher.java
@@ -11,9 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useAccelerometer = false;
-		config.useCompass = false;
-		config.useImmersiveMode = true;
 		initialize(new Mynesweeper(), config);
 	}
 }
