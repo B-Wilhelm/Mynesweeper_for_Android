a38f0486d179fe87369e4fb285f78fa84b7ebf26
Brett <bwilhelm@techie.com>
2021-02-21 02:10:24 -0600

    Set up everything to work with modern versions of gradle, intellij, updated libgdx and plugins, set min platform of android version to 7.1.1, and got rid of a bunch of outdated code that was breaking things.

--- desktop/src/com/tjstudios/mynesweeper/desktop/DesktopLauncher.java
+++ desktop/src/com/tjstudios/mynesweeper/desktop/DesktopLauncher.java
@@ -7,12 +7,6 @@ import com.tjstudios.mynesweeper.Mynesweeper;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Mynesweeper";
-		config.width = 320;
-		config.height = 569;
-		config.vSyncEnabled = false; // Setting to false disables vertical sync
-		config.foregroundFPS = 120; // Setting to 0 disables foreground fps throttling
-		config.backgroundFPS = 120; // Setting to 0 disables background fps throttling
 		new LwjglApplication(new Mynesweeper(), config);
 	}
 }
